3958c590cdcee1cfac81d8a8f7fd19651932e305
Rendy Sebpian <nightcoremosta@gmail.com>
2026-08-08 14:32:17 +0700

    feat: configure Bibata Original Ice cursor

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,8 +1,17 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./labwc.nix
     ./noctalia
   ];
+
+  home.pointerCursor = {
+    name = "Bibata-Original-Ice";
+    package = pkgs.bibata-cursors;
+    gtk.enable = true;
+    x11.enable = true;
+  };
 
   xdg.enable = true;
   wayland.systemd.target = "labwc-session.target";
